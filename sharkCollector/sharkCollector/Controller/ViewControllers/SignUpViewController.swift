5eb3865050f824409fc27cb1a12689d783d7c30e
Jose Melendez <Josem_melendez@icloud.com>
2017-10-10 21:48:13 -0400

    Made connection to the signUpVC

--- sharkCollector/sharkCollector/Controller/ViewControllers/SignUpViewController.swift
+++ sharkCollector/sharkCollector/Controller/ViewControllers/SignUpViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    //MARK: - IB-Outlets
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var confirmationPasswordTextfield: UITextField!
+    
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - IB-Actions
+    
+    @IBAction func createAccountButtonTapped(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }
