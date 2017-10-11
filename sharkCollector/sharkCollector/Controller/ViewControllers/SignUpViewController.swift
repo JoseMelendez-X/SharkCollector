de7a51c8a870c3a07edebcbafd7c50dcdf387233
Jose Melendez <Josem_melendez@icloud.com>
2017-10-10 22:20:33 -0400

    Users can now create an account and sign in

--- sharkCollector/sharkCollector/Controller/ViewControllers/SignUpViewController.swift
+++ sharkCollector/sharkCollector/Controller/ViewControllers/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
@@ -27,6 +28,50 @@ class SignUpViewController: UIViewController {
     
     @IBAction func createAccountButtonTapped(_ sender: UIButton) {
         
+        //Unwrap optionals
+        if let email = emailTextfield.text, let password = passwordTextfield.text, let confirmPassword = confirmationPasswordTextfield.text  {
+            
+            if password == confirmPassword {
+                
+            //Create account/user
+            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                
+                if error != nil {
+                    
+                    //Handle errors here
+                    print(error!.localizedDescription)
+                    
+                } else {
+                    
+                    //Handle successful creation of new user here
+                    
+                    print("Success, new account created")
+                    
+                    //Send user to BorrowersTVC
+                    self.performSegue(withIdentifier: "toBorrowersTVC", sender: self)
+                    
+                }
+                
+            })
+                
+            } else {
+                
+                //Handle password erros here
+                print("Passwords do not match")
+                
+            }
+        }
+        
+    }
+    
+    
+    //MARK: - Functions
+    
+    //touchesBegan
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //Dismiss keyboard
+        self.view.endEditing(true)
     }
     
 }
